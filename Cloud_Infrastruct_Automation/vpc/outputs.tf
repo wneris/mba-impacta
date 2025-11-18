83b0d703c9cee83ae328a8150cd9a9f80003d7cc
Washington Santana <wneris@gmail.com>
2025-11-18 10:45:36 -0300

    add subnets 1c - output

--- Cloud_Infrastruct_Automation/vpc/outputs.tf
+++ Cloud_Infrastruct_Automation/vpc/outputs.tf
@@ -10,12 +10,20 @@ output "subnet_publica_1b_id" {
     value = aws_subnet.public_subnet_1b.id
 }
 
+output "subnet_publica_1c_id" {
+    value = aws_subnet.public_subnet_1b.id
+}
+
 output "subnet_privada_1a_id" {
     value = aws_subnet.private_subnet_1a.id
 }
 
 output "subnet_privada_1b_id" {
     value = aws_subnet.private_subnet_1b.id
+}
+
+output "subnet_privada_1c_id" {
+    value = aws_subnet.private_subnet_1c.id
 }
 
 output "internet_gateway_id" {
@@ -34,16 +42,29 @@ output "private-route-1b_id" {
     value = aws_route_table.private-rt-1b.id
 }
 
+output "private-route-1c_id" {
+    value = aws_route_table.private-rt-1c.id
+}
+
 output "elastic_ip_1a_id" {
     value = aws_eip.eip_1a.id
 }
 output "elastic_ip_1b_id" {
     value = aws_eip.eip_1b.id
 }
+
+output "elastic_ip_1c_id" {
+    value = aws_eip.eip_1c.id
+}
+
 output "natgateway_1a_id" {
     value = aws_nat_gateway.ntg_1a.id
 }
 
 output "natgateway_1b_id" {
     value = aws_nat_gateway.ntg_1b.id
+}
+
+output "natgateway_1c_id" {
+    value = aws_nat_gateway.ntg_1c.id
 }
